58368a4798b76892b9343fdf0d7de5509140ab31
hwayoung lee <hylee13ee@gmail.com>
2024-04-04 22:56:53 +0900

    feat: test

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -9,3 +9,15 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+/*import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store);
+
+Provider 하위로 PersistGate 추가
+〈Provider store={store}〉
+〈PersistGate loading={null} persistor={persistor}〉
+〈App /〉
+〈/PersistGate〉
+〈/Provider〉*/
